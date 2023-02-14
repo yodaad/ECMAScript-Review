a17cd7c81d705e7fde20b4b6c52fadb929a69c45
yodaad@gmail.com <Diego>
2023-02-14 18:24:41 -0500

    String padding adn trailing commas files added

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,4 @@
+const string = "Hello";
+
+console.log(string.padStart(13, "_"));
+console.log(string.padEnd(13, "_"));
---
+++ src/es8/03-trailing-commas.js
@@ -0,0 +1,4 @@
+const array = [24, 34, 15, 54, , , , 45];
+
+console.log(array);
+console.log(array.length);
