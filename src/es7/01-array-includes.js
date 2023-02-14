909b85361108a51b0a50654858b661aebf653993
yodaad@gmail.com <Diego>
2023-02-13 19:22:32 -0500

    es7 folder added as well as exponential and includes js files

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,6 @@
+// Allows to determine if an element is within an array
+let numbers = [1, 3, 4, 7, 19, 22];
+console.log(numbers.includes(7));
+
+const list = ["David", "Oskar", "Anne"];
+console.log(list.includes("Oskar"));
