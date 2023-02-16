a561463d066165dc77725c84ec22d62129ba8d43
yodaad@gmail.com <Diego>
2023-02-16 15:05:09 -0500

    More ES10 files

---
+++ src/es10/00-flat-map.js
@@ -0,0 +1,7 @@
+//  Flat returns a matrix from any submatrix
+const array = [1, 1, 2, 3, 4, [1, 3, 5, 6, [1, 2, 4]]];
+console.log(array.flat(3));
+
+// Flat map
+const array2 = [1, 2, 3, 4, 5];
+console.log(array2.flatMap((v) => [v, v * 2]));
---
+++ src/es10/03-fromentries.js
@@ -0,0 +1,3 @@
+const entries = new Map([["name", "Diego"], ["age, 39"]]);
+console.log(entries);
+console.log(Object.fromEntries(entries));
